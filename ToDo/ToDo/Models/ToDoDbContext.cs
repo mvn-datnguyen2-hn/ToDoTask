162b1c54cf9a458da13f030c0edb8670025a9657
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-09-23 13:42:31 +0700

    Seed data

--- ToDo/ToDo/Models/ToDoDbContext.cs
+++ ToDo/ToDo/Models/ToDoDbContext.cs
@@ -11,8 +11,47 @@ namespace ToDo.Models
         }
         public ToDoDbContext(DbContextOptions<ToDoDbContext> sqlDbContextOptions) : base(sqlDbContextOptions)
         {
-
+            
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    ID = 1,
+                    Name = "Work"
+                },
+                new Category
+                {
+                    ID = 2,
+                    Name = "Family"
+                },
+                new Category
+                {
+                    ID = 3,
+                    Name = "Birth"
+                },
+                new Category
+                {
+                    ID = 4,
+                    Name = "Fun"
+                },
+                new Category
+                {
+                    ID = 5,
+                    Name = "Sport"
+                },
+                new Category
+                {
+                    ID = 6,
+                    Name = "Study"
+                }
+            );
+        }
+
+
         public DbSet<ToDo> Tasks { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<User> Users { get; set; }
